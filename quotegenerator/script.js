47521a7fb0eaff12525d18783b0221e8067d4a6f
Ayushee Yadav <144589455+ayushii89@users.noreply.github.com>
2025-02-18 01:39:51 +0530

    Update script.js

--- quotegenerator/script.js
+++ quotegenerator/script.js
@@ -1,12 +1,12 @@
 const QuoteContainer = document.getElementById("quote");
 const btn = document.getElementById("btn");
-const url = "https://api.quotable.io/random";
+const url = "https://dummyjson.com/quotes/random";
 
 let getQuote = () => {
   fetch(url)
     .then((data) => data.json())
     .then((item) => {
-      QuoteContainer.textContent = `${item.content} - ${item.author}`;
+      QuoteContainer.textContent = `${item.quote} - ${item.author}`;
     })
     .catch((error) => {
       console.error("Error fetching quote:", error);
